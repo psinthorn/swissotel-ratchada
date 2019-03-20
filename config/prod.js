0246eef53c282f995445f2f4ea7c7471f9d093fa
Sinthorn Pradutnam <psinthorn@gmail.com>
2019-03-20 17:37:47 +0700

    Config separate dev and prod version

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,5 @@ module.exports = {
   mongoURI: process.env.MONGODB,
   //Google Key
   googleClientID: process.env.GOOGLE_ID,
-  googleClientSecret: GOOGLE_SECRET
+  googleClientSecret: process.env.GOOGLE_SECRET
 };
